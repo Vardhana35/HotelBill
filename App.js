184c531300bf44d13f93d9982b805444c923dd31
BOBBURI NAGA VARDHANA <126737264+Vardhana35@users.noreply.github.com>
2023-08-14 19:55:27 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+//import logo from './logo.svg';
+import './App.css';
+import HotelBill from './components/HotelBill';
+
+function App() {
+  return (
+    <div className="App">
+      <HotelBill/>
+      </div>)
+}
+
+export default App;
